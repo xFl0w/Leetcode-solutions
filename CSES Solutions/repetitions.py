05d81bb0eff767d4807b6ecfb3df13bd6c7d012d
Henry Hsieh <44388448+xFl0w@users.noreply.github.com>
2020-10-07 22:22:35 -0400

    Update repetitions.py

--- CSES Solutions/repetitions.py
+++ CSES Solutions/repetitions.py
@@ -8,8 +8,8 @@ ans = 0
 r = r'(.)\1+'
 mx = sorted((m.group() for m in re.finditer(r, s)))
 for i in mx:
-	temp = len(i)
-	ans = max(temp, ans)
+    temp = len(i)
+    ans = max(temp, ans)
 if ans == 0:
-	ans+=1
+    ans+=1
 print(ans)
