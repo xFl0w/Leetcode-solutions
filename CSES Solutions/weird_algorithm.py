230f53aaff0d8518a793e6a7700a679820473c33
Henry Hsieh <44388448+xFl0w@users.noreply.github.com>
2020-10-07 22:22:14 -0400

    Update weird_algorithm.py

--- CSES Solutions/weird_algorithm.py
+++ CSES Solutions/weird_algorithm.py
@@ -4,14 +4,14 @@ ans = []
 ans.append(n)
  
 while n > 1:
-	if n%2 == 0:
-		temp = n//2
-		n = temp
-		ans.append(temp)
+    if n%2 == 0:
+        temp = n//2
+	n = temp
+	ans.append(temp)
  
-	elif n%2 == 1:
-		temp = n*3+1
-		n = temp
-		ans.append(temp)
+    elif n%2 == 1:
+	temp = n*3+1
+	n = temp
+	ans.append(temp)
  
 print(*ans)
