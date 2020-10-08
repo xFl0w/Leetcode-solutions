5c33f939fc78c6361d98f83d362a7059512335dc
Henry Hsieh <44388448+xFl0w@users.noreply.github.com>
2020-10-07 22:16:30 -0400

    Create weird_algorithm.py

---
+++ CSES Solutions/weird_algorithm.py
@@ -0,0 +1,17 @@
+n = int(input())
+ 
+ans = []
+ans.append(n)
+ 
+while n > 1:
+	if n%2 == 0:
+		temp = n//2
+		n = temp
+		ans.append(temp)
+ 
+	elif n%2 == 1:
+		temp = n*3+1
+		n = temp
+		ans.append(temp)
+ 
+print(*ans)
