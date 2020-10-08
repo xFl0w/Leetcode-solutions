aae8d915590f0129f0fdc01a24bb209a67ab2d16
Henry Hsieh <44388448+xFl0w@users.noreply.github.com>
2020-10-07 23:13:46 -0400

    Update height_checker.py

--- leetcode-easy-questions/height_checker.py
+++ leetcode-easy-questions/height_checker.py
@@ -1,8 +1,8 @@
 class Solution:
-	def heightChecker(self, heights):
-		c = 0
-		a = sorted(heights)
-		for i in range(len(heights)):
-			if heights[i] != a[i]:
-				c+=1
-		return c
+    def heightChecker(self, heights):
+        c = 0
+        a = sorted(heights)
+        for i in range(len(heights)):
+            if heights[i] != a[i]:
+                c+=1
+        return c
