3f521bc93024c6abeed570041601b4d884bfb73d
Henry Hsieh <44388448+xFl0w@users.noreply.github.com>
2020-10-07 22:20:31 -0400

    Create increasing_array.py

---
+++ CSES Solutions/increasing_array.py
@@ -0,0 +1,15 @@
+import sys
+import re
+import math
+from collections import *
+ 
+n = int(input())
+l = list(map(int, input().strip().split()))
+ 
+c = 0
+for i in range(len(l)-1):
+	if l[i+1] < l[i]:
+		temp = abs(l[i+1]-l[i])
+		c+=temp
+		l[i+1] = l[i]
+print(c)
